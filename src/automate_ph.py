cc741ec5460d255725f3e2079f080edc0d0fcf67
Gabriel Martins <gabriel_alves_m@outlook.com.br>
2021-07-26 12:00:51 -0300

    updated

--- src/automate_ph.py
+++ src/automate_ph.py
@@ -2,13 +2,13 @@ import win32com.client
 import os 
 #from pathvalidate import sanitize_filename
 
-class Automate_ph:
+class AutomatePh:
   app = None
   psd_file = None
 
   def __init__(self):
     self.app = win32com.client.Dispatch("Photoshop.Application")
-    self.app.Visible = True 
+    self.app.Visible = False 
   
   def closePhotoshop(self):
     self.app.Quit()
@@ -16,10 +16,11 @@ class Automate_ph:
   def openPSD(self, filename):
     if os.path.isfile(filename) == False:
       self.closePhotoshop()
-      return False
-    
+      return False   
+
     self.app.Open(filename)
     self.psd_file = self.app.Application.ActiveDocument
+    
     return True
   
   def closePSD(self):
@@ -31,22 +32,21 @@ class Automate_ph:
   def updateLayer(self, Layer_name, text):
     if self.psd_file is None:
       raise Exception(FileExistsError)
-    
+
     layer = self.psd_file.ArtLayers[Layer_name]
     layer_text = layer.TextItem
     layer_text.contents = text
+    
     return True
 
   def exportJPEG(self, filename, folder='', quality=10):
     if self.psd_file is None:
       raise Exception(FileExistsError)
-    
-    full_path = os.path.join(folder, filename)
 
+    full_path = os.path.join(folder, filename)
     options = win32com.client.Dispatch("Photoshop.ExportOptionsSaveForWeb")
     options.Format = 6
     options.Quality = quality
-
     self.psd_file.Export(ExportIn=full_path, ExportAs=2, Options=options)
 
     return os.path.isfile(full_path)
--- tests/tests.py
+++ tests/tests.py
@@ -3,64 +3,62 @@ import unittest
 
 sys.path.insert(0, './src')
 
-from automate_ph import Automate_ph
+from automate_ph import AutomatePh
 
 class TestPh(unittest.TestCase):
 
-    def setUp(self):
-      
-        self.psd_origin = os.path.abspath('./tests/resources/test.psd')
-        self.jpeg_path = os.path.abspath('./tests/resources/tmp')
-        self.jpeg_name = "test-01.jpg"
-        self.jpeg_full_path = os.path.join(self.jpeg_path, self.jpeg_name)
+  def setUp(self):
+    self.psd_origin = os.path.abspath('./tests/resources/test.psd')
+    self.jpeg_path = os.path.abspath('./tests/resources/tmp')
+    self.jpeg_name = "test-01.jpg"
+    self.jpeg_full_path = os.path.join(self.jpeg_path, self.jpeg_name)
 
-        if not os.path.exists(self.jpeg_path):
+    if not os.path.exists(self.jpeg_path):
           os.mkdir(self.jpeg_path)
 
-        self.app = Automate_ph()
+    self.app = AutomatePh()
 
-    def tearDown(self):
-      self.app.closePhotoshop()
+  def tearDown(self):
+    self.app.closePhotoshop()
 
-      # if os.path.exists(self.jpeg_full_path):
-      #   os.remove(self.jpeg_full_path)
+    if os.path.exists(self.jpeg_full_path):
+      os.remove(self.jpeg_full_path)
 
-      if os.path.exists(self.jpeg_path):
-        os.rmdir(self.jpeg_path)
+    if os.path.exists(self.jpeg_path):
+      os.rmdir(self.jpeg_path)
 
-    def test_openPSD(self):
-      opened = self.app.openPSD(self.psd_origin)
-      if opened:
-        self.app.closePSD()
-      self.assertTrue(opened)
+  def test_openPSD(self):
+    opened = self.app.openPSD(self.psd_origin)
+    if opened:
+      self.app.closePSD()
+    self.assertTrue(opened)
     
-    def test_updateLayer(self):
-      updated1 = False
-      updated2 = False
+  def test_updateLayer(self):
+    updated1 = False
+    updated2 = False
 
-      opened = self.app.openPSD(self.psd_origin)
-      if opened:
-          updated1 = self.app.updateLayer("Palavra", "Hello")
-          updated2 = self.app.updateLayer("Frase", "Hello World!")
-          self.app.closePSD()
+    opened = self.app.openPSD(self.psd_origin)
+    if opened:
+      updated1 = self.app.updateLayer("Palavra", "Hello")
+      updated2 = self.app.updateLayer("Frase", "Hello World!")
+      self.app.closePSD()
 
-      self.assertTrue(updated1)
-      self.assertTrue(updated2)
+    self.assertTrue(updated1)
+    self.assertTrue(updated2)
     
-      def test_exportJPEG(self):
-        exported = False
+  # def test_exportJPEG(self):
+  #   exported = False
 
-        opened = self.app.openPSD(self.psd_origin)
-        if opened:
-            updated1 = self.app.updateLayer("Palavra", "Hello")
-            updated2 = self.app.updateLayer("Frase", "Hello World!")
+  #   opened = self.app.openPSD(self.psd_origin)
+  #   if opened:
+  #     updated1 = self.app.updateLayer("Palavra", "Hello")
+  #     updated2 = self.app.updateLayer("Frase", "Hello World!")
 
-            if updated1 & updated1:
-              exported = self.app.exportJPEG
+  #     if updated1 & updated1:
+  #       exported = self.app.exportJPEG
+  #       self.app.closePSD()
 
-            self.app.closePSD()
-
-        self.assertTrue(exported)
+  #   self.assertTrue(exported)
 
 if __name__ == '__main__':
   unittest.main()
